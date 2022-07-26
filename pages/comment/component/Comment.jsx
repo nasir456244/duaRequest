e9992816f0a4d5c41b0612841135262573cc6af7
nasir456244 <arturlegendflame@gmail.com>
2022-07-26 23:39:20 +1000

    finished

--- pages/comment/component/Comment.jsx
+++ pages/comment/component/Comment.jsx
@@ -30,13 +30,13 @@ const Comment = ({ address, comment, createdAt, name, image,id, deleteDua, setDe
 
   
 
-  const queryId = window.location.pathname.split("/")[2];
-
+  
   const deleteComment = (id) => {
     if(!auth?.currentUser) return
     if(!owner) return
     setIsDeleteModalOpen(true)
     if(deleteDua === true) {
+      const queryId = window.location.pathname.split("/")[2];
       const commentToDelete = id
       deleteDoc(doc(db,'Prayers', queryId, 'comments', commentToDelete))
       setDeleteDua(false)
