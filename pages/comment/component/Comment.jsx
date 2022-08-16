73590ac3cd87c7c1c1bed37e2b7f866524c79bb8
nasir456244 <arturlegendflame@gmail.com>
2022-08-16 12:01:59 +1000

    added like/dislike comment

--- pages/comment/component/Comment.jsx
+++ pages/comment/component/Comment.jsx
@@ -154,7 +154,7 @@ const Comment = ({ address, comment, createdAt, name, image, id }) => {
                 />
                 )}
             <div className={styles.buttons}>
-                <AiFillLike onClick={likecomment} className={`${user && isPaidAccount && hasliked ? 'text-[#0ABEEE]' : 'hover:scale-125 transition-all duration-150 ease-out text-[#ADADAD]'}  relative bottom-[3px] cursor-pointer `} size={25} /> {likes?.length > 0 && 
+                <AiFillLike onClick={likecomment} className={`${user && isPaidAccount && hasliked && 'text-[#0ABEEE]'} ${user && 'hover:scale-125 transition-all duration-150 ease-out'}  text-[#ADADAD] relative bottom-[3px] cursor-pointer `} size={25} /> {likes?.length > 0 && 
                   <p className="text-[13px] font-semibold bg-opacity-100 ml-1">
                     {likes?.length > 0 ? Intl.NumberFormat("en", { notation: "compact" }).format(likes?.length) : '0' }
                   </p>
