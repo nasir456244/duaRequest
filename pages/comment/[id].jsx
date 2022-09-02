45e45959c7c1cd4f7281874066a11263274f3bd4
nasir456244 <arturlegendflame@gmail.com>
2022-09-02 10:47:03 +1000

    added next-seo

--- pages/comment/[id].jsx
+++ pages/comment/[id].jsx
@@ -162,12 +162,6 @@ const CommentPage = () => {
 
   return (
     <div>
-      <Head>
-        <title>Prayer request</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Navbar />
       {user && isPaidAccount &&
         <div className={styles.mainContainer}>
