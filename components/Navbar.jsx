16ac5d27f166cd7cf07cc67e5528995584c4506b
nasir456244 <arturlegendflame@gmail.com>
2022-09-12 16:05:10 +1000

    free trial for 150 users

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ import Icon from '@/public/icon.png';
 import { MdAccountCircle } from "react-icons/md";
 
 const styles = {
-  container: `w-full h-[100px] sm:justify-between flex justify-around items-center p-[12px] overflow-hidden`,
+  container: `w-full mt-10 h-[100px] sm:justify-between flex justify-around items-center p-[12px] overflow-hidden`,
   logo: `lg:text-[1.8rem] md:text-[1.4rem] text-[#fff] flex items-center justify-center `,
   item: `sm:border-b-2 sm:border-[#0ABEEE]`,
   list: `md:flex hidden items-center justify-center xs:fixed xs:bottom-[20px] lg:static`,
@@ -18,6 +18,7 @@ const styles = {
   menuContainer: `md:hidden flex text-white`,
   ListMenuContainer: `min-w-[30vw] min-h-[100vh] top-[0] items-center right-[0] flex fixed overflow-hidden z-40 bg-gradient-to-tr from-violet-700 via-cyan-600 to-green-300`,
   OpenMenuContainer: ` w-full h-full flex flex-col items-center justify-center`,
+  trial: `fixed top-0 font-medium text-[#000] text-[20px] z-50 bg-[#ffcc00] w-full flex justify-center p-3`,
 };
 
 const Navbar = () => {
@@ -25,8 +26,11 @@ const Navbar = () => {
   const router = useRouter();
   const { user } = useContext(PrayerRequestContext)
   const isPaidAccount = user?.stripeRole !== "free"
-
   return (
+    <>
+    <div className={styles.trial}>
+        <p>Free trial offer for the first 150 users.</p>
+    </div>
     <div className={styles.container}>
       <div className={styles.logo}>
         <div className="flex items-center justify-center">
@@ -89,13 +93,13 @@ const Navbar = () => {
         </div>
       </div>
       {isMenuOpen && (
-        <div className={styles.ListMenuContainer}>
+        <div className= {styles.ListMenuContainer}>
           <RiMenu3Fill
             size={30}
             onClick={() => {
               isMenuOpen && setIsMenuOpen(false);
             }}
-            className="text-white cursor-pointer transition-all duration-500 hover:scale-110 flex absolute left-[10px] top-[10px]"
+            className='mt-10 text-white cursor-pointer transition-all duration-500 hover:scale-110 flex absolute left-[10px] top-[10px]'
           />
           <div className={styles.OpenMenuContainer}>
 
@@ -137,6 +141,7 @@ const Navbar = () => {
         </div>
       )}
     </div>
+    </>
   );
 };
 
